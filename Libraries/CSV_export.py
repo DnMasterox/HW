796f5a6990a559454a58c31db81c3a23534f1bfa
mshumako <mastershumakov@gmail.com>
2019-12-02 18:59:14 +0200

    Some refactoring made

--- Libraries/CSV_export.py
+++ Libraries/CSV_export.py
@@ -7,13 +7,13 @@ __all__ = ['clear_file', 'append_file']
 
 @keyword('Clear file')
 def clear_file(filepath):  # Clear_file :it will clear your file.
-    with open(filepath, 'w+') as f:
-        obj1 = csv.writer(f)
+    with open(filepath, 'w+') as file:
+        writer = csv.writer(file)
 
 
 @keyword('Append file')
 def append_file(filepath, data):  # Append_file :it will append the data you wanted to in your script.
     with open(filepath, 'a') as file:
         # csv.Dialect.delimiter = delimiter
-        obj1 = csv.writer(file, delimiter='|', quoting=csv.QUOTE_NONE, escapechar=' ')
-        obj1.writerow(data)
+        writer = csv.writer(file, delimiter='|', quoting=csv.QUOTE_NONE, escapechar=' ')
+        writer.writerow(data)
